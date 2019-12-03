2f480fd6e747e81bfee488c1b3bc4ca9fd33c280
Aleksandr Miedviediev <MyStockWork2017@gmail.com>
2019-12-03 22:44:16 +0200

    Урок forEach

--- script/script.js
+++ script/script.js
@@ -1288,3 +1288,68 @@
 // }
 
 // t5();
+
+// ---Unit 18. Методы массивов: join, split, forEach(часть 2)
+// Task 1.
+// Дан массив a1 = [4, 5, 6, 7, 12, 34, 56, 78, 90, 11] - с помощью forEach переберите массив и создайте новый массив a1_res куда добавьте элементы данного массива умноженные на 2. Действия должны запускаться при вызове функции t1.
+
+// let a1 = [4, 5, 6, 7, 12, 34, 56, 78, 90, 11];
+// let a1_res = [];
+// let out = document.querySelector(".out");
+
+// function t1() {
+//     a1.forEach(function (item) {
+//         a1_res.push(item * 2);
+//     })
+//     out.innerHTML = a1_res;
+// }
+// t1();
+// console.log(a1_res);
+
+// -----------------Task 2---------------------
+// Дан массив a2 = [2, 3, 4, 5, 10, 11, 12] - с помощью forEach переберите массив и создайте новый массив a2_res куда добавьте элементы данного массива деленные на 2. Действия должны запускаться при вызове функции t2.
+
+// let a2 = [2, 3, 4, 5, 10, 11, 12];
+// let a2_res = [];
+
+// function t2() {
+//     a2.forEach(function (item) {
+//         a2_res.push(item / 2);
+//     })
+//     out.innerHTML = a2_res;
+// }
+// t2();
+// console.log(a2_res);
+
+// ----------------------Task 3---------------------
+// Дан массив a3 = [2, "hello", 3, "hi", 4, "Mazai"] - с помощью forEach переберите массив и создайте новый массив a3_res куда добавьте элементы данного массива являющиеся числом.Запускаться решение должно при вызове функции t3.
+// let a3 = [2, "hello", 3, "hi", 4, "Mazai"];
+// let a3_res = [];
+// let out = document.querySelector(".out");
+
+// function t3() {
+//     a3.forEach(function (item) {
+//         a3_res.push(parseInt(item));
+//     })
+//     out.innerHTML = a3_res;
+// }
+// t3();
+// console.log(a3_res);
+
+// ------------------------Task 4------------------
+// На странице созданы 3 span.task - 4 c атрибутом data.С помощью forEach переберите их и добавьте атрибуты в массив a4_res Запускаться решение должно при вызове функции t4.
+
+let task = document.querySelectorAll('.task-4');
+let data = task.getAttribute('data');
+
+let a4_res = [];
+
+console.log(data);
+// function t4() {
+//     task.forEarch(function (item) {
+//         a4_res.push(item);
+//     })
+//     console.log(a4_res);
+// }
+
+// t4();
